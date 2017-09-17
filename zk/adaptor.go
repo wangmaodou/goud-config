75d274eb6dae9392f26118765291d7900e01f6d8
maodou <1261014323@qq.com>
2017-09-17 23:32:20 +0800

    change to adaptor.

--- zk/adaptor.go
+++ zk/adaptor.go
@@ -3,19 +3,19 @@ package zoke
 import "fmt"
 
 var (
-	Reactor=Impl{}
+	Reactor=new(Impl)
 )
 
 type Impl struct{
 	CallBack
 }
 
-func (c Impl) OnDataChange(path string,client *ZkClient){
+func (i Impl) OnDataChange(path string,client *ZkClient){
 	fmt.Println("onDataChange======>",path)
 	fmt.Println(client.GetNodeData(path))
 }
 
-func (c Impl) OnChildNodeChange(path string,client *ZkClient){
+func (i Impl) OnChildNodeChange(path string,client *ZkClient){
 	fmt.Println("OnChildNodeChange======>",path)
 	fmt.Println(client.GetChildNodes(path))
 
